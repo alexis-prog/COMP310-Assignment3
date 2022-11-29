283f19e9b691f26e81ce423c6caaf9e60b3f8d3e
Alexis VIAU-CARDINAL <aviauc@teach-node-03.cs.mcgill.ca>
2022-11-29 06:14:44 -0500

    Helper functions
    Should be done with helper functions.
    Currently manages dynamically:
    * INodes
    * Blocks

--- Code/sfs_api.c
+++ Code/sfs_api.c
@@ -8,62 +8,306 @@
 #include "sfs_api.h"
 #include "disk_emu.h"
 
-typedef uint8_t byte;
+#define MAGIC_NUMBER 0xABCD0005
+#define BLOCK_SIZE 1024
+#define NUM_BLOCKS 2048
 
-char* disk_name = "disk";
-uint32_t magic = 0xABCD0005;
-uint32_t block_size = 1024;
-uint32_t num_blocks = 1024;
+#define BLOCK_CACHE_SIZE 16
+#define INODE_CACHE_SIZE 16
+
+// Dynamic Constants
+#define INODE_SIZE 64
+#define INODES_PER_BLOCK (BLOCK_SIZE / INODE_SIZE)
+
+#define DIR_ENTRY_SIZE 64
+#define DIR_ENTRIES_PER_BLOCK (BLOCK_SIZE / DIR_ENTRY_SIZE)
+
+// Never trust char size
+typedef uint8_t byte_t;
+
+char* disk_name = "disk.sfs";
+
+// Fixed size of 1024 bytes
+typedef struct _block_t{
+    byte_t data[BLOCK_SIZE];
+} block_t;
+
+// INODE - 64 bytes -> 16 per block
+typedef struct _inode_t {
+    uint32_t mode;
+    uint32_t link_count;
+    uint32_t size;
+
+    uint32_t direct[12];
+    uint32_t indirect;
+} inode_t;
 
 
-typedef struct superblock {
+// Fixed size of 64 bytes -> 16 per block
+typedef struct _dir_entry_t {
+    byte_t valid;
+    uint32_t inode;
+    char filename[64 - sizeof(byte_t) - sizeof(uint32_t)];
+} dir_entry_t;
+
+// Superblock representation (fixed at 1024 byte for ease of use)
+typedef struct _superblock_t {
     uint32_t magic;
     uint32_t block_size;
     uint32_t file_system_size;
     uint32_t inode_table_length;
     uint32_t root_dir_inode;
-} superblock;
+    byte_t padding[BLOCK_SIZE - 5*sizeof(uint32_t)];
+} superblock_t;
 
-typedef struct inode {
-    byte mode;
-    byte link_count;
-    byte uid;
-    byte gid;
 
-    uint32_t direct[12];
-    uint32_t indirect;
-} inode;
+// In-memory
+superblock_t *superblock = NULL;
 
-// Initialization helper functions
-void write_inode(inode* node, int index){
-    byte* buffer = calloc(1, block_size);
-    memcpy(buffer, node, sizeof(inode));
-    write_blocks(index, 1, buffer);
-    free(buffer);
+// Block Cache
+block_t block_cache[BLOCK_CACHE_SIZE];
+uint32_t block_cache_index[BLOCK_CACHE_SIZE];
+uint16_t block_cache_age[BLOCK_CACHE_SIZE];
+uint16_t block_rolling_counter = 0;
+
+// Inode Cache
+inode_t inode_cache[INODE_CACHE_SIZE];
+uint32_t inode_cache_index[INODE_CACHE_SIZE];
+uint16_t inode_cache_age[INODE_CACHE_SIZE];
+uint16_t inode_rolling_counter = 0;
+
+// Block cache management
+uint32_t get_oldest_block(){
+    uint32_t oldest_index = -1;
+    for(int i = 0; i < BLOCK_CACHE_SIZE; i++){
+        if(block_cache_index[i] == -1){
+            return i;
+        }
+        if(block_cache_age[i] < block_cache_age[oldest_index]){
+            oldest_index = i;
+        }
+    }
+    write_blocks(block_cache_index[oldest_index], 1, block_cache[oldest_index].data);
+    block_rolling_counter++;
+    return oldest_index;
 }
 
-void init_superblock(){
-    byte* superblock = calloc(block_size, sizeof(byte));
+void _write_block(uint32_t block_num, block_t* block){
 
-    // Set magic number
-    memcpy(superblock, &magic, 4);
+    for(int i = 0; i < BLOCK_CACHE_SIZE; i++){
+        if(block_cache_index[i] == block_num){
+            memcpy(block_cache[i].data, block->data, BLOCK_SIZE);
+            block_cache_age[i] = block_rolling_counter;
+            return;
+        }
+    }
 
-    // Set block size
-    memcpy(superblock+4, block_size, 4);
+    unsigned int oldest = get_oldest_block();
 
-    // Set number of blocks
-    memcpy(superblock+8, num_blocks, 4);
+    if(oldest == -1){
+        printf("Error: Failed to find oldest block in cache\n");
+        return;
+    }
 
-    // Set iNode table size
-    memcpy(superblock+12, 1, 4);
+    // Update cache
+    memcpy(block_cache[oldest].data, block->data, BLOCK_SIZE);
+    block_cache_index[oldest] = block_num;
+    block_cache_age[oldest] = block_rolling_counter;
+}
 
-    // Set root iNode
-    memcpy(superblock+16, 0, 4);
+void _read_block(uint32_t block_num, block_t* block){
+
+    for(int i = 0; i < BLOCK_CACHE_SIZE; i++){
+        if(block_cache_index[i] == block_num){
+            memcpy(block->data, block_cache[i].data, BLOCK_SIZE);
+            block_cache_age[i] = block_rolling_counter;
+            return;
+        }
+    }
+
+    unsigned int oldest = get_oldest_block();
+
+    if(oldest == -1){
+        printf("Error: Failed to find oldest block in cache");
+    }
+
+    // Update cache
+    read_blocks(block_num, 1, block->data);
+    memcpy(block_cache[oldest].data, block->data, BLOCK_SIZE);
+    block_cache_index[oldest] = block_num;
+    block_cache_age[oldest] = block_rolling_counter;
+}
+
+int is_block_free(uint32_t block_num){
+    uint32_t block_index = block_num / 8 / BLOCK_SIZE;
     
-    // Write superblock to disk
-    write_blocks(0, 1, superblock);
+    block_t block;
+    _read_block(superblock->file_system_size - 1 -  block_index, &block);
 
-    free(superblock);
+   return (block.data[block_num / 8 % BLOCK_SIZE] & (1 << (block_num % 8))) == 0;
+}
+
+void set_block_status(uint32_t block_num, int status){
+    uint32_t block_index = block_num / 8 / BLOCK_SIZE;
+    
+    block_t block;
+    _read_block(superblock->file_system_size - 1 -  block_index, &block);
+
+    if(status == 1){
+        block.data[block_num / 8 % BLOCK_SIZE] |= (1 << (block_num % 8));
+    } else {
+        block.data[block_num / 8 % BLOCK_SIZE] &= ~(1 << (block_num % 8)); // inverted bitmask to clear bit
+    }
+}
+
+uint32_t get_free_block(){
+    int num_free_block = NUM_BLOCKS / 8 / BLOCK_SIZE;
+
+    for(int i = num_free_block; i >= 0; i--){
+        block_t block;
+        _read_block(superblock->file_system_size - 1 - i, &block);
+
+        for(int j = 0; j < BLOCK_SIZE; j++){
+            if(block.data[j] != 0xFF){
+                for(int k = 0; k < 8; k++){
+                    if((block.data[j] & (1 << k)) == 0){
+                        return i * BLOCK_SIZE * 8 + j * 8 + k;
+                    }
+                }
+            }
+        }
+    }
+
+    printf("Error: No free blocks\n");
+    exit(1);
+}
+
+// I-Node management
+void write_inode_to_disk(uint32_t inode_num, inode_t* inode){
+    uint32_t block_num = inode_num / INODES_PER_BLOCK;
+
+    if(block_num > superblock->inode_table_length){
+        if(is_block_free(block_num)){
+            set_block_status(block_num, 1);
+            superblock->inode_table_length++;
+            _write_block(0, (block_t*)superblock);
+        } else {
+            printf("Error: Block %d is not free, failed contiguous allocation of i-node table\n", block_num);
+            return;
+        }
+    }
+
+    block_t block;
+    _read_block(block_num + 1, &block);
+
+    memcpy(block.data + (inode_num % INODES_PER_BLOCK) * sizeof(inode_t), inode, sizeof(inode_t));
+}
+
+uint32_t get_oldest_inode(){
+    uint32_t oldest_index = -1;
+    for(int i = 0; i < INODE_CACHE_SIZE; i++){
+        if(inode_cache_index[i] == -1){
+            return i;
+        }
+        if(inode_cache_age[i] < inode_cache_age[oldest_index]){
+            oldest_index = i;
+        }
+    }
+
+    uint32_t inode_block_num = inode_cache_index[oldest_index] / INODES_PER_BLOCK;
+
+    for(int i = 0; i < INODE_CACHE_SIZE; i++){
+        if(inode_cache_index[i] / INODES_PER_BLOCK == inode_block_num){
+            write_inode_to_disk(inode_cache_index[i], &inode_cache[i]);
+        }
+    }
+
+    inode_rolling_counter++;
+    return oldest_index;
+}
+
+void get_inode(uint32_t inode_num, inode_t* inode){
+    for(int i = 0; i < INODE_CACHE_SIZE; i++){
+        if(inode_cache_index[i] == inode_num){
+            inode_cache_age[i] = inode_rolling_counter;
+            return i;
+        }
+    }
+
+    unsigned int oldest = get_oldest_inode();
+
+    if(oldest == -1){
+        printf("Error: Failed to find oldest inode in cache\n");
+        exit(1);
+    }
+
+    uint32_t inode_block_num = inode_num / INODES_PER_BLOCK;
+
+    block_t block;
+    _read_block(inode_block_num + 1, &block);
+
+    memcpy(&inode_cache[oldest], block.data + (inode_num % INODES_PER_BLOCK) * sizeof(inode_t), sizeof(inode_t));
+    memcpy(inode, block.data + (inode_num % INODES_PER_BLOCK) * sizeof(inode_t), sizeof(inode_t));
+    inode_cache_index[oldest] = inode_num;
+    inode_cache_age[oldest] = inode_rolling_counter;
+
+    inode_rolling_counter++;
+}
+
+uint32_t get_next_free_inode(){
+    for(int i = 0; i < superblock->inode_table_length; i++){
+        block_t block;
+        _read_block(i + 1, &block);
+
+        for(int j = 0; j < INODES_PER_BLOCK; j++){
+            inode_t* inode = (inode_t*)(block.data + j * sizeof(inode_t));
+            if(inode->link_count == 0){
+                return i * INODES_PER_BLOCK + j;
+            }
+        }
+    }
+
+    return superblock->inode_table_length * INODES_PER_BLOCK + 1;
+}
+
+void write_inode(inode_t* node, uint32_t index){
+    uint32_t block_num = index / INODES_PER_BLOCK;
+    if(block_num > superblock->inode_table_length){
+        if(block_num + 1 != superblock->inode_table_length){
+            printf("Error: Failed contiguous allocation of i-node table\n");
+            return;
+        }
+        if(is_block_free(block_num+1)){
+            set_block_status(block_num, 1);
+            superblock->inode_table_length++;
+        } else {
+            printf("Error: Block %d is not free, failed contiguous allocation of i-node table\n", block_num);
+            return;
+        }
+    }
+
+    uint32_t oldest = get_oldest_inode();
+    memcmp(&inode_cache[oldest], node, sizeof(inode_t));
+    inode_cache_index[oldest] = index;
+    inode_cache_age[oldest] = inode_rolling_counter;
+
+    inode_rolling_counter++;
+}
+
+
+// Initialization helper functions
+void init_superblock(){
+    superblock->magic = MAGIC_NUMBER;
+    superblock->block_size = BLOCK_SIZE;
+    superblock->file_system_size = NUM_BLOCKS;
+    superblock->inode_table_length = 1;
+    superblock->root_dir_inode = 0;
+
+    write_blocks(0, 1, (void *) superblock);
+}
+
+void init_free_list(){
+
 }
 
 
@@ -71,9 +315,21 @@ void init_superblock(){
 // API functions
 void mksfs(int fresh)
 {
+    // Invalidate cache upon initialization
+    for(int i = 0; i < BLOCK_CACHE_SIZE; i++){
+        block_cache_index[i] = -1;
+    }
+    
+    for(int i = 0; i < INODE_CACHE_SIZE; i++){
+        inode_cache_index[i] = -1;
+    }
+    
+
+    superblock = calloc(1, sizeof(superblock_t));
+
     if (fresh == 1)
     {
-        if(init_fresh_disk(disk_name, block_size, num_blocks)){
+        if(init_fresh_disk(disk_name, BLOCK_SIZE, NUM_BLOCKS)){
             printf("Error: Could not create new disk file - Aborting %s\n\n", disk_name);
             exit(1);
         }
@@ -84,7 +340,7 @@ void mksfs(int fresh)
     }
     else
     {
-        if(init_disk(disk_name, block_size, num_blocks)){
+        if(init_disk(disk_name, BLOCK_SIZE, NUM_BLOCKS)){
             printf("Error: Could not open disk file - Aborting %s\n\n", disk_name);
             exit(1);
         }
